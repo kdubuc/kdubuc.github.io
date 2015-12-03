c3f734c1f7c3cbe3e9836727e849250bc7718d2f
Kévin DUBUC <kevindubuc62@gmail.com>
2015-12-03 16:36:47 +0100

    Version 2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
 
-  var actions = [
-    'play video games',
-    'jog',
-    'create apps',
-    'check twitter',
-    'buy compulsively on amazon'
-  ];
+    var actions = [
+        'play video games',
+        'jog',
+        'create apps',
+        'check twitter',
+        'buy compulsively on amazon'
+    ];
 
-  actions.push($('#do').text());
+    actions.push($('#do').text());
 
-  $('#do').cycle(actions, {
-    delay: 5000,
-    transitionDuration: 500,
-    transitionEasing: 'swing'
-  });
+    $('#do').cycle(actions, {
+        delay: 5000,
+        transitionDuration: 500,
+        transitionEasing: 'swing'
+    });
+
+    $(window).resize($.throttle(300, function() {
+        var pattern = Trianglify({
+            width: $(window).innerWidth(),
+            height: $(window).innerHeight(),
+            cell_size: 120,
+            seed: 0.7888697197195143,
+            x_colors: ['#6794b1', '#395b65', '#9165a0']
+        });
+
+        $('body').css('background-image', 'url('+pattern.png()+')');
+    })).resize();
 
 });
